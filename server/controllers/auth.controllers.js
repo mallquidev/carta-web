faf91451190cb83b9ec6ffe8a42b816e8db49668
mallquidev <159455849+mallquidev@users.noreply.github.com>
2024-07-20 20:44:55 -0500

    register

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,4 +1,12 @@
+import { PASSWORD } from '../config.js'
 import {pool} from '../db.js'
 
-export const register = (req, res) => res.send('REGISTRANDO')
+export const register = async(req, res) => {
+    try {
+        const {user, email, password} = req.body
+        const [result] = await pool.query('INSERT INTO user(user, email, password) VALUES(?,?,?)',[user, email, password])
+    } catch (error) {
+        
+    }
+}
 export const login = (req, res) => res.send('LOGIN')
